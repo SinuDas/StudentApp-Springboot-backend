d41f9a7e928a7edf5caedf11d75b1d11515d7c27
SinuDas <68272072+SinuDas@users.noreply.github.com>
2020-07-23 21:28:17 +0530

    Update StudentServiceImpl.java

--- src/main/java/com/spring/fullstackproject/service/StudentServiceImpl.java
+++ src/main/java/com/spring/fullstackproject/service/StudentServiceImpl.java
@@ -6,40 +6,30 @@ import com.spring.fullstackproject.entity.StudentData;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class StudentServiceImpl implements StudentService {
-    private StudentDataRepository strd;
-
+    private StudentDataRepository Objectrepository;
+    //ConstructorInjection -here the object of StudentDataRepository(interface) will be injected
     @Autowired
     public StudentServiceImpl( StudentDataRepository theStudentDataRepository) {
-        strd = theStudentDataRepository;
+    	Objectrepository = theStudentDataRepository;
     }
-
+     //it is a method to save data,methods from spring data jpa used here like save,findByOrderByNameAsc() methods;
     public void Save(StudentData student){
-       strd.save(student);
-       
+    	Objectrepository.save(student);
+       //save is a method from hibernate and Spring data jpa
     }
-//    public List<StudentData> findAllOrderByName() {
-//    	List<StudentData> student=strd.findAllOrderByName();
-//    	return student;
-//    	
-//    }
 
 	@Override
 	public List<StudentData> findByOrderByNameAsc() {
-   	List<StudentData> student=strd. findByOrderByNameAsc();
+   	List<StudentData> student=Objectrepository. findByOrderByNameAsc();
 
 		return student;
-	}
-    
-
-	
-	}
-
+    }
+}
 	
 
 	
