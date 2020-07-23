e8b9e66b4082fbe2154bf914d5b9191bea1dfc2c
SinuDas <68272072+SinuDas@users.noreply.github.com>
2020-07-23 21:25:28 +0530

    Update StudentController.java

--- src/main/java/com/spring/fullstackproject/controller/StudentController.java
+++ src/main/java/com/spring/fullstackproject/controller/StudentController.java
@@ -6,7 +6,6 @@ import com.spring.fullstackproject.service.StudentService;
 
 import java.util.List;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,31 +13,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
- @CrossOrigin("http://localhost:8081") 
+@CrossOrigin("http://localhost:8081") 
 @RequestMapping("/api")
 public class StudentController {
-     private StudentService std;
+   //Constructor Injection-used to inject object of service
+     private StudentService Objectservice;
 
     @Autowired
     public StudentController(StudentService theStudentService) {
-        std = theStudentService;
+    	Objectservice = theStudentService;
     }
 
 
-
+   // Method to save data from api to DataBase, linked to service and serviceimpl 
     @PostMapping("/StudentDatas")
-    public ResponseEntity<StudentData>add(@Valid @RequestBody StudentData student) {
-
-      std.Save(student);
-       
-		return  new ResponseEntity<>(student, HttpStatus.CREATED);
-
+    public ResponseEntity<StudentData>add(@RequestBody StudentData student) {
+   //ResponseEntity used to get Http response
+    Objectservice.Save(student);
+    return  new ResponseEntity<>(student, HttpStatus.CREATED);
     }
+    
+    //Method to find Data from DataBase,linked to service and serviceimpl
     @GetMapping("/StudentDatas")
     public ResponseEntity<List<StudentData>> find(){
     	
-         List<StudentData> student= std.findByOrderByNameAsc();    	
-    	return  new ResponseEntity<>( student,HttpStatus.OK);
+     List<StudentData> student= Objectservice.findByOrderByNameAsc();    	
+     return  new ResponseEntity<>( student,HttpStatus.OK);
     }
 
 }
