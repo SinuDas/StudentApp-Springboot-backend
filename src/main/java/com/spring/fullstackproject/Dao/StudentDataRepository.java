37ca601fb58dd9009517a458ba8d9f29d32762ce
SinuDas <68272072+SinuDas@users.noreply.github.com>
2020-07-23 21:29:15 +0530

    Update StudentDataRepository.java

--- src/main/java/com/spring/fullstackproject/Dao/StudentDataRepository.java
+++ src/main/java/com/spring/fullstackproject/Dao/StudentDataRepository.java
@@ -7,12 +7,11 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-/*@CrossOrigin("http://localhost:8081")*/
+
 @Repository
 public interface StudentDataRepository extends MongoRepository<StudentData, String>{
 
-
+//SpringDataJpa Method to findData from Table by ascending order of name
 	List<StudentData> findByOrderByNameAsc();
 }
